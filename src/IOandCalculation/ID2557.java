9a3c27da8cfa18266234cea4a2e092acbbf6b96a
Dan <dankim0213@gmail.com>
2020-11-28 13:17:22 +0900

    2557 2588

---
+++ src/IOandCalculation/ID2557.java
@@ -0,0 +1,9 @@
+// Hello World 
+package IOandCalculation;
+
+class ID2557 {
+    public static void main(String[] args) {
+        String i = "123";
+        System.out.println(i + "Hello World!");
+    }
+}
---
+++ src/IOandCalculation/ID2588.java
@@ -0,0 +1,35 @@
+// 곱셈
+package IOandCalculation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class ID2588 {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader r1 = new InputStreamReader(System.in);
+        InputStreamReader r2 = new InputStreamReader(System.in);
+        BufferedReader br1 = new BufferedReader(r1);
+        BufferedReader br2 = new BufferedReader(r2);
+
+        String aStr = br1.readLine(); int aNum = Integer.parseInt(aStr); 
+        String bStr = br2.readLine(); int bNum = Integer.parseInt(bStr); 
+
+        int first = aNum;
+        int second = bNum;
+        int third = bNum%10 /1 * aNum;
+        int fourth = bNum%100 /10 * aNum;
+        int fifth = bNum /100 * aNum;
+        int sixth = aNum * bNum;
+        
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
+        System.out.println(fifth);
+        System.out.println(sixth);
+    }
+}
+
+
+// Need to be more abstractically!
