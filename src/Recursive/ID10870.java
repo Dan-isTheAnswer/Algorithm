72bdd4a62d7e960b71a62db04d48ab1839ea7ec8
Dan <dankim0213@gmail.com>
2020-12-02 21:40:13 +0900

    10870

--- src/Recursive/ID10870.java
+++ src/Recursive/ID10870.java
@@ -4,42 +4,21 @@ package Recursive;
 import java.util.Scanner;
 
 class ID10870 {
-    private static int[] arrFib;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt(); // input
+        int n = sc.nextInt();
         sc.close();
 
-
-        int ans = doFibonacci(num);
-        System.out.println(ans);
-
+        System.out.println(doFibonacci(n));
     }
 
-    private static int doFibonacci(int index) {
-        preprocess(index); 
-
-        if (index < 2) {
-            return arrFib[index];
+    public static int doFibonacci(int n) {
+        if (n <2) {
+            return n;
         }
-
-        for (int i = 2; i < index+1; i++) {
-            arrFib[i] = arrFib[i-2] + arrFib[i-1];
-        }
-
-        return arrFib[index];
-
-    }
-
-    private static void preprocess(int index) {
-        if (index < 2) {
-            arrFib = new int[2]; 
-        } else {
-            arrFib = new int[index+1];
-        }
-        arrFib[0] = 0;
-        arrFib[1] = 1;
-    }
+        return doFibonacci(n-1) + doFibonacci(n-2);
+    } 
 }
-// I approached this problem with respect to memory.
+// I approached this problem with respect to a mathmetic concept.
+// The fibonacci number consists of 
+// 0 from the index of 0 and 1 from the index of 1.
