3e1d27545f41497334f17f4eef25c76afa3a8fa0
Dan <dankim0213@gmail.com>
2020-12-16 14:11:47 +0900

    add a print fuction to see the array while RodCutting

--- src/DP/RodCutting.java
+++ src/DP/RodCutting.java
@@ -1,31 +1,44 @@
-package DivideAndConquer;
+package DP;
+
+import java.util.Arrays;
 
 /**
- * A DynamicProgramming solution for Rod cutting problem Returns the best obtainable price for a rod
- * of length n and price[] as prices of different pieces
+ * A DynamicProgramming solution for Rod cutting problem Returns the best
+ * obtainable price for a rod of length n and price[] as prices of different
+ * pieces
  */
 class RodCutting {
 
-  private static int cutRod(int[] price, int n) {
-    int val[] = new int[n + 1];
-    val[0] = 0;
+    private static int[] cutRod(int[] price, int n) {
+        int[] val = new int[n+1];
+        val[0] = 0;
 
-    for (int i = 1; i <= n; i++) {
-      int max_val = Integer.MIN_VALUE;
-      for (int j = 0; j < i; j++) max_val = Math.max(max_val, price[j] + val[i - j - 1]);
+        for (int i = 1; i <= n; i++) {
+            int max_val = Integer.MIN_VALUE;
+            for (int j = 0; j < i; j++) max_val = Math.max(max_val, price[j] + val[i-1 -j]);
 
-      val[i] = max_val;
+            val[i] = max_val;
+        }
+
+        return val;
     }
 
-    return val[n];
-  }
+    public static void main(String[] args) {
+        // Let's see the whole result!!
+        int[] arr = new int[] {2, 5, 13, 19, 20};
+        for (int arrSize = 1; arrSize<= arr.length; arrSize++) {
+            int[] limitedArr = Arrays.copyOf(arr, arr.length);
+            Arrays.fill(limitedArr, arrSize, arr.length, 0);
 
-  // main function to test
-  public static void main(String args[]) {
-    int[] arr = new int[] {2, 5, 13, 19, 20};
-    int size = arr.length;
-    int result = cutRod(arr, size);
-    System.out.println("Maximum Obtainable Value is " + result);
-  }
+            System.out.println("Item " + arrSize + " available: ");
+            int n = 5; // price length. 
+            int[] result = cutRod(limitedArr, n);
+            for (int i=1; i<arr.length+1; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
 }
 
