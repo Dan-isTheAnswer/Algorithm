79ba128308cbc0f4dafc8e0b23cf949ba7642eab
Dan <dankim0213@gmail.com>
2020-12-28 15:02:19 +0900

    modified ID2178 to submit to BJ

--- src/Graph/ID2178.java
+++ src/Graph/ID2178.java
@@ -1,47 +1,48 @@
 // https://www.acmicpc.net/problem/2178
 package Graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 // Shortest Path(BFS)
 class ID2178 {
+    private static int n, m;
+
     // 0: west; 1: south; 2: east; 3: north;
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, -1, 0, 1};
+    private static int[] dx = { -1, 0, 1, 0 };
+    private static int[] dy = { 0, -1, 0, 1 };
     private static int[][] maze; // (y, x)
     private static int[][] ans;
 
-
     private static int bfs(int desty, int destx) {
-        int[] src = new int[]{0, 0};
+        int[] src = new int[] { 0, 0 };
 
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        boolean[][] visited = new boolean[n][m];
         Queue<int[]> q = new ArrayDeque<>();
 
         int[] turn = src;
         q.add(turn);
         visited[turn[0]][turn[1]] = true;
-        System.out.println(turn[0] + " " + turn[1]);
+        // System.out.println(turn[0] + " " + turn[1]);
 
-        
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             turn = q.poll();
 
-            // Check out four directions to go. 
+            // Check out four directions to go.
             for (int i = 0; i < 4; i++) {
                 int nY = turn[0] + dy[i];
                 int nX = turn[1] + dx[i];
-                if (nY >= 0 && nX >= 0 && 
-                    nY <= 6 && nX <= 6 &&
-                    visited[nY][nX] != true && 
-                    maze[nY][nX] != 0) {
-                    
+                if (nY >= 0 && nX >= 0 && nY < n && nX < m && visited[nY][nX] != true && maze[nY][nX] != 0) {
+
                     ans[nY][nX] += ans[turn[0]][turn[1]];
 
-                    q.add(new int[]{nY, nX});
+                    q.add(new int[] { nY, nX });
                     visited[nY][nX] = true;
-                    System.out.println(turn[0] + " " + turn[1]);
+                    // System.out.println(turn[0] + " " + turn[1]);
                 }
 
             }
@@ -51,35 +52,30 @@ class ID2178 {
         return ans[desty][destx];
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        maze = new int[n][m];
+        ans = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                maze[i][j] = line.charAt(j) - '0'; // ascii
+                ans[i][j] = line.charAt(j) - '0';
+            }
+        }
         
-        maze = new int[][]{
-                           {1, 0, 1, 1, 1, 1, 1},
-                           {1, 1, 1, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 1, 1, 1, 1, 1, 1}};
-
-        ans = new int[][]{
-                           {1, 0, 1, 1, 1, 1, 1},
-                           {1, 1, 1, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 0, 0, 0, 0, 0, 1},
-                           {1, 1, 1, 1, 1, 1, 1}};
-        int result = bfs(6, 6);
+        int result = bfs(n-1, m-1);
         System.out.println(result);
         
-        System.out.println("Hello");
 
     }
 }
-// If you want to implement XY chart programmatically, 
+// Rod-column set is programmatically different from x-y set. 
+// BFS's visited could be true in two ways: when either adding or poll. 
 /**
  * If you want to implement a XY chart programmatically,
- * Think of for-loop order i.e. (y, x).
+ * Think of for-loop orders i.e. (y, x).
  */
