6d158f5d12a1d6e6a3c6f1b99f8996f366e87ec3
Dan <dankim0213@gmail.com>
2020-12-30 14:45:06 +0900

    modified ID2839 to submit to BJ

--- src/Greedy/ID2839.java
+++ src/Greedy/ID2839.java
@@ -7,64 +7,44 @@ class ID2839tmp4 {
     public static void main(String[] args) {
         int[] N = {18, 4, 6, 9, 11, 22};
         for (int i : N) {
-            int a  = checkValidity(i);
-            System.out.println("i is " + i + "; validity is " + a);
-            System.out.println("Result : " + howManyPackOfSugar(a, i));
+            System.out.println("Result : " + howManyPackOfSugar(i));
 
         }
+        // Scanner sc = new Scanner(System.in);
+        // int N = sc.nextInt();
+        // sc.close();
+
+
+        // int result = howManyPackOfSugar(N);
+        // System.out.println(result);
     }
 
-    private static int howManyPackOfSugar(int validity, int n) {
+    public static int howManyPackOfSugar(int n) {
         int ans = 0;
+        boolean isMixed = false;
 
-        if (validity == 1) {
+        if (n % 5 == 0) {
             ans = n/5;
-        } else if (validity == 2) {
+            return ans;
+        } else {
+
             int temp = n;
-            int tempoftemp = 0;
+            int tmpoftmp = 0;
             for (int i = 0; i < n/3 && temp > 0; i++) {
                 temp -= 5;
-                if (temp % 3 == 0 && temp >0) {
+                
+                if (temp % 3 == 0 && temp > 0) {
+                    isMixed = true;
+                    tmpoftmp= temp;
                     ans = i+1;
-                    tempoftemp = temp;
                 }
             }
-            ans = ans + tempoftemp/3;
-        } else if (validity == 3) {
-            ans = n/3;
-        } else {
-            ans = -1;
-        }
+            ans = ans + tmpoftmp/3;
+        } 
 
-        return ans;
-
-    }
-
-    private static int checkValidity(int n) {
-        int validity = 0;
-        
-        // case 1: n % 5 == 0; case 2: mixed; case 3: n % 3 ==0;
-        if (n % 5 == 0) {
-            validity = 1;
-            return validity;
-        }
-         
-        int temp = n;
-        for (int i = 0; i < n/3; i++) {
-            temp -= 5;
-            if (temp % 3 == 0 && temp >0) {
-                validity = 2;
-                return validity;
-            }
-        }
-
-        if (n % 3 == 0) {
-            validity = 3;
-            return validity;
-        }
-
-        validity = -1;
-        return validity;
+        if (isMixed) return ans;
+        else if (n % 3 == 0) return n/3;
+        else return -1;
     }
 
 }
