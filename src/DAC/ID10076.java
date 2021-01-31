b8b10f07d80ed22f02e1776d50c5b03b25b2044f
Dan <dankim0213@gmail.com>
2021-01-31 16:21:07 +0900

    failed: DAC[ID10076]

--- src/DAC/ID10076.java
+++ src/DAC/ID10076.java
@@ -1,9 +1,8 @@
 // https://www.acmicpc.net/problem/10076
 package DAC;
 
-import java.util.Arrays;
-
-// failed: Exceed Memory Limit...
+// Tried Bitmasking but...
+// failed: Exceed Time Limit...
 class ID10076 {
     private static int n;
     private static int start;
@@ -13,37 +12,42 @@ class ID10076 {
     public static void main(String[] args) {
         n = 5;
         start = 2;
+        // d = 8; // r + 30 + l + 20 + l + 2 + l + 10
         d = 7;
         tourList = new int[] {10, 2, 20, 30, 1}; // n times
-        // int[] tourList = {10, 1, 30, 2, 20}; // I think the answer is 52 but error occurred...
-        boolean[] visited = new boolean[n];
+        // int[] tourList = {10, 1, 30, 2, 20}; // the answer is 52 but error occurred...
+        // boolean[] visited = new boolean[n];
 
+        int toured = 0; // **bitmasking 
         ID10076 tour = new ID10076();
-        int result = tour.tour(tourList, 1, start, visited, 0);
+        int result = tour.tour(0, start, toured, 0);
         System.out.println(result);
     }
 
-    public int tour(int[] tourList, int cnt, int cur, boolean[] visited,int val) {
-        if (d < cnt) 
+    public int tour(int cnt, int curr, int toured,int val) {
+        if (d == cnt) 
             return val; // base case
 
-        boolean[] isVisited = Arrays.copyOf(visited, visited.length);
+        // this causes memory exceeding. 
+        // boolean[] isVisited = Arrays.copyOf(visited, visited.length);
+        
         // to the left
         int prev = 0;
-        if (cur > 0) {
-            prev = tour(tourList, cnt+1, cur-1, isVisited, val);
+        if (curr > 0) {
+            prev = tour(cnt+1, curr-1, toured, val);
         } 
 
-        int here = 0;
-        if (!isVisited[cur]) {
-            isVisited[cur] = true;
-            here = tour(tourList, cnt+1, cur, isVisited, val+tourList[cur]);
+        int here = 0; // | 
+        if ((toured | 1 << n - curr) != toured) { 
+            // isVisited[cur] = true;
+            int isToured = toured | 1 << n - curr;
+            here = tour(cnt+1, curr, isToured, val+tourList[curr]);
         }
 
         // to the right
         int next = 0;
-        if (cur < tourList.length-1) {
-            next = tour(tourList, cnt+1, cur+1, isVisited, val);
+        if (curr < tourList.length-1) {
+            next = tour(cnt+1, curr+1, toured, val);
         }
 
         int maxVal = Integer.max(prev, Integer.max(here, next));
