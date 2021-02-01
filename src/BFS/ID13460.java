fb714f9a7c81914c45ba44af162e5580d86374b1
Dan <dankim0213@gmail.com>
2021-02-01 13:04:13 +0900

    BFS[ID13460]

--- src/BFS/ID13460.java
+++ src/BFS/ID13460.java
@@ -2,115 +2,157 @@
 package BFS;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 
-// TODO: move Blue ball simultaneously. 
 class ID13460 {
-    private int[][] map;
-    private int[] dy = {-1, 0, 1, 0};
-    private int[] dx = {0, -1, 0, 1};
+    // east west south north
+    private static final int[] dy = { 0, 0, 1, -1 };
+    private static final int[] dx = { 1, -1, 0, 0 };
+
+    private static class Snapshot {
+        int ry;
+        int rx;
+        int by;
+        int bx;
+        int cnt;
+
+        public Snapshot(int ry, int rx, int by, int bx, int cnt) {
+            this.ry = ry;
+            this.rx = rx;
+            this.by = by;
+            this.bx = bx;
+            this.cnt = cnt;
+        }
+    }
 
     public static void main(String[] args) {
-        int N = 7;
-        int M = 7;
+        int N = 3;
+        int M = 10;
+        String[] inputs = {"##########", "#.O....RB#", "##########"};
 
-        String[] inputs = {"#######",
-                           "#..R#B#",
-                           "#.#####",
-                           "#.....#",
-                           "#####.#",
-                           "#O....#",
-                           "#######"};
+        // int N = 10;
+        // int M = 10;
+        // String[] inputs = {"##########", "#R#...##B#", "#...#.##.#", "#####.##.#", "#......#.#", "#.######.#", "#.#....#.#", "#.#.##...#", "#O..#....#", "##########"};
+
+        // int N = 7;
+        // int M = 7;
+        // String[] inputs = {"#######", "#...RB#", "#.#####", "#.....#", "#####.#", "#O....#", "#######" };
+
         // int N = 5; // row
         // int M = 5; // col
-
-        // String[] inputs = {"#####", 
-        //                    "#..B#",
-        //                    "#.#.#",
-        //                    "#RO.#",
-        //                    "#####"};
-
-        ID13460 balls = new ID13460();
-        int ans = balls.solve(N, M, inputs);
-        System.out.println(ans);
-
-        
-    }
-
-    public int solve(int N, int M, String[] inputs) {
-        this.map = new int[N][M];
-        int[] zero = conversion(inputs, map);
-        for (int[] arr : map) {
-            System.out.println(Arrays.toString(arr));
-        }
-        System.out.println(Arrays.toString(zero));
-        System.out.println();
-
-        int result = bfs(zero);
-        for (int[] arr : map) {
-            System.out.println(Arrays.toString(arr));
-        }
-
-        return result;
-    }
-
-    public int bfs(int[] zero) {
-        int N = this.map.length;
-        int M = this.map[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][M];
-        int[] src = new int[4];
+        // String[] inputs = {"#####", "#..B#","#.#.#","#RO.#","#####"};
+        //
+        char[][] map = new char[N][M];
+        int[] red = new int[2]; 
+        int[] blue = new int[2];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (this.map[i][j] == 1) src = new int[]{i, j, 0, 0};
-            }
-        }
-        
-        q.add(src);
-        visited[src[0]][src[1]] = true;
-        while(!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int y = tmp[0];
-            int x = tmp[1];
-            int previousDirY = tmp[2];
-            int previousDirX = tmp[3];
-            // visited[y][x] = true;
-
-            for (int i = 0; i < 4; i++) {
-                int nY = y + dy[i];
-                int nX = x + dx[i];
-                if (nY >= 0 && nY < N && nX >= 0 && nX < M) {
-                    if (!visited[nY][nX] && map[nY][nX] != -1 && map[nY][nX] != -2) {
-                        if ((previousDirY == 0 && previousDirX == 0) ||
-                            (previousDirY == dy[i] && previousDirX == dx[i])) {
-                            this.map[nY][nX] = this.map[y][x];
-                        } else {
-                            this.map[nY][nX] = this.map[y][x] + 1;
-                        }
-
-                        q.add(new int[]{nY, nX, dy[i], dx[i]});
-                        visited[nY][nX] = true;
-                    }
+                map[i][j] = inputs[i].charAt(j);
+                if (map[i][j] == 'R') {
+                    red[0] = i; red[1] = j;
+                } else if (map[i][j] == 'B') {
+                    blue[0] = i; blue[1] = j;
                 }
             }
         }
 
-        return this.map[zero[0]][zero[1]];
+        bfs(map, red, blue);
     }
 
-    // R is 1, B is -2 but B is moved 
-    public int[] conversion(String[] inputs, int[][] map) {
-        int[] zero = new int[2];
-        for (int i = 0; i < inputs.length; i++) {
-            for (int j = 0; j < inputs[i].length(); j++) {
-                if (inputs[i].charAt(j) == '#') map[i][j] = -1;
-                else if (inputs[i].charAt(j) == 'R') map[i][j] = 1;
-                else if (inputs[i].charAt(j) == 'B') map[i][j] = -2;
-                else if (inputs[i].charAt(j) == 'O') zero = new int[] {i, j};
+    private static void bfs(char[][] map, int[] srcRed, int[] srcBlue) {
+
+        boolean isRedGoaled = false;
+        boolean isBlueGoaled = false;
+        int n = map.length; int m = map[0].length;
+        Queue<Snapshot> q = new ArrayDeque<>();
+        boolean[][][][] visited = new boolean[n][m][n][m];
+
+        q.offer(new Snapshot(srcRed[0], srcRed[1], srcBlue[0], srcBlue[1], 1));
+
+        while (!q.isEmpty()) {
+            Snapshot snap = q.poll();
+            visited[snap.ry][snap.rx][snap.by][snap.bx] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int[] nr = new int[] {snap.ry + dy[i], snap.rx + dx[i]};
+                int[] nb = new int[] {snap.by + dy[i], snap.bx + dx[i]};
+                // red
+                int[] tmpRed = new int[] {snap.ry, snap.rx}; // this is the legal value which meets the condition
+                while (map[nr[0]][nr[1]] != '#') { // check and then confirm
+                    tmpRed[0] = nr[0]; tmpRed[1] = nr[1]; // copy value 
+                    // fin!
+                    if (map[nr[0]][nr[1]] == 'O') {
+                        isRedGoaled = true;
+                        break;
+                    }
+                    
+                    nr[0] += dy[i]; nr[1] += dx[i]; // update the ref's value
+                }
+                // blue
+                int[] tmpBlue = new int[] {snap.by, snap.bx};
+                while (map[nb[0]][nb[1]] != '#') {
+                    tmpBlue[0] = nb[0]; tmpBlue[1] = nb[1];
+                    // skip!
+                    if (map[nb[0]][nb[1]] == 'O') {
+                        isBlueGoaled = true;
+                        break;
+                    }
+
+                    nb[0] += dy[i]; nb[1] += dx[i];
+                }
+
+                // if red and blue are overlapped each other
+                if (tmpRed[0] == tmpBlue[0] && tmpRed[1] == tmpBlue[1]) {
+                    if (isRedGoaled && isBlueGoaled) {
+                        // skip 
+                    } else if (i == 0) { // east
+                        if (snap.rx < snap.bx) 
+                            tmpRed[1] -= 1;
+                        else 
+                            tmpBlue[1] -= 1;
+                    } else if (i == 1){
+                        if (snap.rx < snap.bx) 
+                            tmpBlue[1] += 1;
+                        else 
+                            tmpRed[1] += 1;
+                    } else if (i == 2) {
+                        if (snap.ry < snap.by) 
+                            tmpRed[0] -= 1;
+                        else 
+                            tmpBlue[0] -= 1;
+                    } else if (i == 3) {
+                        if (snap.ry < snap.by) 
+                            tmpBlue[0] += 1;
+                        else 
+                            tmpRed[0] += 1; 
+                    }
+
+                }
+                // check the red ball is only goaled. If not, reset.
+                if (isRedGoaled && !isBlueGoaled){
+                    break;
+                } else if (!isRedGoaled && isBlueGoaled) {
+                    isBlueGoaled = false;
+                    continue; 
+                } else if (isRedGoaled && isBlueGoaled) {
+                    isRedGoaled = false;
+                    isBlueGoaled = false;
+                    continue;
+                }
+                
+                // q.offer
+                if (!visited[tmpRed[0]][tmpRed[1]][tmpBlue[0]][tmpBlue[1]] && snap.cnt <10) { 
+                    q.offer(new Snapshot(tmpRed[0], tmpRed[1], tmpBlue[0], tmpBlue[1], snap.cnt+1));
+                }
+
+            }
+            // print ans and break the while-loop 
+            if (isRedGoaled && !isBlueGoaled) {
+                System.out.println(snap.cnt);
+                break;
             }
         }
-
-        return zero;
+        if (!isRedGoaled) System.out.println(-1);
+        
     }
 }
